8b691cc4110e51a653fa2a3dc709bef13f517ac2
GDAKamil <zawadzki.kamil2@gmail.com>
2018-10-16 23:29:39 +0200

    poprawka dla detekcji kolizji

--- Paletkowo/Sprites/Sprite.cs
+++ Paletkowo/Sprites/Sprite.cs
@@ -43,33 +43,36 @@ namespace Paletkowo.Sprites
         protected bool IsTouchingLeft(Sprite sprite)
         {
             return this.Rectangle.Right + this.Velocity.X > sprite.Rectangle.Left &&
-                this.Rectangle.Left < sprite.Rectangle.Left &&
-                this.Rectangle.Bottom > sprite.Rectangle.Top &&
-                this.Rectangle.Top < sprite.Rectangle.Bottom;
+              this.Rectangle.Left < sprite.Rectangle.Left &&
+              this.Rectangle.Bottom > sprite.Rectangle.Top &&
+              this.Rectangle.Top < sprite.Rectangle.Bottom;
         }
+
         protected bool IsTouchingRight(Sprite sprite)
         {
             return this.Rectangle.Left + this.Velocity.X < sprite.Rectangle.Right &&
-                this.Rectangle.Right > sprite.Rectangle.Right &&
-                this.Rectangle.Bottom > sprite.Rectangle.Top &&
-                this.Rectangle.Top < sprite.Rectangle.Bottom;
+              this.Rectangle.Right > sprite.Rectangle.Right &&
+              this.Rectangle.Bottom > sprite.Rectangle.Top &&
+              this.Rectangle.Top < sprite.Rectangle.Bottom;
         }
+
         protected bool IsTouchingTop(Sprite sprite)
         {
-            return this.Rectangle.Right + this.Velocity.X > sprite.Rectangle.Left &&
-                this.Rectangle.Left < sprite.Rectangle.Left &&
-                this.Rectangle.Bottom > sprite.Rectangle.Top &&
-                this.Rectangle.Top < sprite.Rectangle.Bottom;
+            return this.Rectangle.Bottom + this.Velocity.Y > sprite.Rectangle.Top &&
+              this.Rectangle.Top < sprite.Rectangle.Top &&
+              this.Rectangle.Right > sprite.Rectangle.Left &&
+              this.Rectangle.Left < sprite.Rectangle.Right;
         }
-        //protected bool IsTouchingBottom(Sprite sprite)
-        //{
-        //    return this.Rectangle.Top + this.Velocity.Y < sprite.Rectangle.Bottom &&
-        //        this.Rectangle.Bottom > sprite.Rectangle.Bottom &&
-        //        this.Rectangle.Right > sprite.Rectangle.Left &&
-        //        this.Rectangle.Left < sprite.Rectangle.Right;
-        //}
-        #endregion
 
+        protected bool IsTouchingBottom(Sprite sprite)
+        {
+            return this.Rectangle.Top + this.Velocity.Y < sprite.Rectangle.Bottom &&
+              this.Rectangle.Bottom > sprite.Rectangle.Bottom &&
+              this.Rectangle.Right > sprite.Rectangle.Left &&
+              this.Rectangle.Left < sprite.Rectangle.Right;
+        }
+
+        #endregion
 
     }
 }
